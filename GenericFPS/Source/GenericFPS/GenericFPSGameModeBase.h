29d7110c7783728c360e9c380357659e07620b78
Bryce Olcott <10872719@uvu.edu>
2023-05-15 04:18:48 -0600

    Created FPS game project

---
+++ GenericFPS/Source/GenericFPS/GenericFPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GenericFPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GENERICFPS_API AGenericFPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
